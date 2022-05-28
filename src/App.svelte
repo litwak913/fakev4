b24c495d1022fd28c4bbccb74ca235841e05dd71
litwak913 <litwak913@qq.com>
2022-05-28 18:58:33 +0800

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import {Nav} from 'svelte-chota'
+	export let name: string;
+</script>
+
+<header>
+	<Nav class="nav">
+
+	</Nav>
+</header>
+
+<style>
+	header {
+		background-color: #4caf50;
+		width: 100%;
+		height: 10%;
+		padding:0;
+		box-shadow: 2px 0px 4px #000000;
+	}
+	.nav {
+		width: 60%;
+		margin: 0 0 auto;
+	}
+</style>
